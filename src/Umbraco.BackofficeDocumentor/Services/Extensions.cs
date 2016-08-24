649f06f33b088de3e340b37138b4120e0598814f
doronuziel71 <doronuziel@gmail.com>
2016-08-24 07:06:11 +0300

    Refactoring Razor View

--- src/Umbraco.BackofficeDocumentor/Services/Extensions.cs
+++ src/Umbraco.BackofficeDocumentor/Services/Extensions.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using Umbraco.BackofficeDocumentor.Models;
 
 namespace Umbraco.BackofficeDocumentor.Services
 {
@@ -21,6 +24,28 @@ namespace Umbraco.BackofficeDocumentor.Services
                 }
             }
             return false;
+        }
+
+        public static string FormatId(this VisualizerContentTypeModel model)
+        {
+            return string.Format("cmp_{0}", model.Id);
+        }
+
+        public static IHtmlString ContentTypeLink(this HtmlHelper html, VisualizerContentTypeModel model, object htmlAttributes=null)
+        {
+            var tb=new TagBuilder("a");
+            tb.MergeAttribute("class","small");
+            tb.MergeAttribute("href","#" + model.FormatId());
+            tb.InnerHtml = model.Name;
+
+            if (htmlAttributes != null)
+            {
+                var dictionary = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+                tb.MergeAttributes(dictionary,true);
+            }
+            
+
+            return new MvcHtmlString(tb.ToString());
         }
     }
 }
